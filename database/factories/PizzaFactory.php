c13bacb88822a9b36ca5540b96867a259769fa2c
Bunny <bunnycdy@gmail.com>
2023-12-19 22:18:41 +0545

    pizza seeded

--- database/factories/PizzaFactory.php
+++ database/factories/PizzaFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Arr;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Pizza>
@@ -16,8 +17,39 @@ class PizzaFactory extends Factory
      */
     public function definition(): array
     {
+        $toppingChoices = [
+            'Extra Cheese',
+            'Pepperoni',
+            'Mushrooms',
+            'Onions',
+            'Sausage',
+            'Green Peppers',
+            'Black Olives',
+            'Bacon',
+            'Ham',
+            'Pineapple',
+            'Spinach',
+            'Tomatoes',
+            'Jalapeños',
+            'Chicken',
+            'Ground Beef',
+            'Feta Cheese',
+        ];
+
+        $toppings = [];
+
+        for ($i=0; $i <= rand(1,5); $i++) { 
+            $toppings[] = Arr::random($toppingChoices);
+        }
+
+        $toppings = array_unique($toppings);
+        
         return [
-            //
+            'user_id' => rand(11,10),
+            'size' => Arr::random(['Small','Medium', 'Large', 'Extra-Large']),
+            'crust' => Arr::random(['Normal','Thin','Hand-tossed','Pan','Stuffed crust','Garlic']),
+            'toppings' => $toppings,
+            'status' => Arr::random(['Ordered', 'Preparing', 'Baking', 'Cooking', 'Ready for pickup', 'Delivered']),
         ];
     }
 }
